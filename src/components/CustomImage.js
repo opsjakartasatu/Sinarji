a15236abfb8b7c69a27d182420780078e0446bc2
opsjakartasatu <operasionaljakartasatu@gmail.com>
2025-08-27 23:01:26 +0700

    Update Image

--- src/components/CustomImage.js
+++ src/components/CustomImage.js
@@ -1,7 +1,18 @@
-import Image from 'next/image';
+// import Image from "next/image";
+
+// const CustomImage = ({ src, alt, width, height, priority = false, ...props }) => {
+//   const basePath = process.env.BASE_PATH;
+//   const imageSrc = `${basePath}${src}`;
+
+//   return <Image src={imageSrc} alt={alt} width={width} height={height} priority={priority} {...props} />;
+// };
+
+// export default CustomImage;
+
+import Image from "next/image";
 
 const CustomImage = ({ src, alt, width, height, priority = false, ...props }) => {
-  const basePath = process.env.BASE_PATH;
+  const basePath = process.env.__NEXT_ROUTER_BASEPATH || "";
   const imageSrc = `${basePath}${src}`;
 
   return <Image src={imageSrc} alt={alt} width={width} height={height} priority={priority} {...props} />;
