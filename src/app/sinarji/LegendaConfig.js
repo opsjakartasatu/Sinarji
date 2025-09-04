0f58b02968feed4e5415ec02c8c222aee4b7099b
opsjakartasatu <operasionaljakartasatu@gmail.com>
2025-09-04 10:45:13 +0700

    Update LegendaConfig.js

--- src/app/sinarji/LegendaConfig.js
+++ src/app/sinarji/LegendaConfig.js
@@ -1,34 +1,28 @@
-// LegendConfig.js
-
 const legends = {
-  Elevasi: [
-    {
-      title: "Di bawah laut",
-      gradient: "#5D9FF1",
-    },
-    {
-      title: "Di atas laut",
-      gradient: "linear-gradient(to right, #000000, #ffffff)", // hitam → putih
-    },
-  ],
-  // "Penurunan Tanah": children: Laju[
-  //   {
-  //     title: "Laju -15 cm/th",
-  //     gradient: "linear-gradient(to right, #ffff00, #ff0000)",
-  //   },
-  //   {
-  //     title: "Laju -8 cm/th",
-  //     gradient: "linear-gradient(to right, #ffff00, #ff0000)",
-  //   },
-  //   {
-  //     title: "Laju -4 cm/th",
-  //     gradient: "linear-gradient(to right, #ffff00, #ff0000)",
-  //   },
-  //   {
-  //     title: "Laju 0 cm/th",
-  //     gradient: "linear-gradient(to right, #ffff00, #ff0000)",
-  //   },
-  // ],
+  Elevasi: {
+    "Referensi High Tide": [
+      { title: "Di bawah laut", gradient: "linear-gradient(to right, #0995FC, #052943)" },
+      { title: "Di atas laut", gradient: "linear-gradient(to right, #090F0D, #9F9D9B)" },
+    ],
+    "Referensi MSL": [
+      { title: "Di bawah laut", gradient: "linear-gradient(to right, #0A7FD4, #0C2C3D)" },
+      { title: "Di atas laut", gradient: "linear-gradient(to right, #090F0D, #9F9D9B)" },
+    ],
+  },
+  "Penurunan Tanah": {
+    Laju: [
+      { title: "Laju -15 cm/th", gradient: "#CB150D" },
+      { title: "Laju -8 cm/th", gradient: "#DADB17" },
+      { title: "Laju -4 cm/th", gradient: "#6D82A8" },
+      { title: "Laju 0 cm/th", gradient: "#072852" },
+    ],
+    Magnitude: [
+      { title: "Magnitude -3.0 m", gradient: "#CB150D" },
+      { title: "Magnitude -2.0 m", gradient: "#DADB17" },
+      { title: "Magnitude -1.0 m", gradient: "#6D82A8" },
+      { title: "Magnitude 0 m", gradient: "#072852" },
+    ],
+  },
 };
 
 export default legends;
